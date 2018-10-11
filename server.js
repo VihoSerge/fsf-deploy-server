be36a876b66ca73a2d88bf80637b7e02ac314f76
franky007 <serge.adigo@ism.edu.sn>
2018-10-11 15:18:36 +0000

    B

--- server.js
+++ server.js
@@ -4,12 +4,29 @@ let bodyParser = require('body-parser');
 let http = require('http');
 let mongoose = require('mongoose');
 
+let cors = require('cors');
+
 // Configure mongoose to use promise
 mongoose.Promise = require('bluebird');
 
 let resgisRoutes = require('./routes').default;
 
+var originsWhitelist = [
+  'http://localhost:4200',  
+  'https://fsf-app.herokuapp.com'
+];
+
+var corsOptions = {
+  origin: function(origin, callback){
+        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+  },
+  credentials:true
+}
+
 const app = express();
+
+app.use(cors(corsOptions));
 
 // Connect to the database before starting the application server.
 mongoose.connect(process.env.MONGODB_ADDON_URI || 'mongodb://localhost/fsfapp').then(
@@ -28,5 +45,5 @@ resgisRoutes(app);
 
 // Initialize the app.
 server.listen(process.env.PORT || 3000, () => {
-    console.log(`Serveur demarrer avec succes au port :${3000}`);
+    console.log(`Serveur demarrer avec suc0ces au port :${3000}`);
 });
